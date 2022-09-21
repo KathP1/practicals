7575b034a6c5bc8ad89a39001cb6e953e77b17bb
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-21 23:29:52 +1000

    Add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,11 +2,16 @@
 Program to determine score status
 """
 
-
+import random
 def main():
     score = float(input("Enter score: "))
     score_status = determine_score_status(score)
     print(score_status)
+    print("\n")
+    score = random.randint(0,100)
+    score_status = determine_score_status(score)
+    print("The random score of", score, "is", score_status)
+
 
 
 def determine_score_status(score):
