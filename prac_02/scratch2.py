52f97a40cdbe615760a5388c1c66f5b3b6e518c5
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-22 11:37:58 +1000

    Move MENU outside main()

--- prac_02/scratch2.py
+++ prac_02/scratch2.py
@@ -8,21 +8,18 @@ Q - quit
 
 from math import floor
 
-MENU = """G - get score 
+
+def main():
+    MENU = """G - get score
 P - print result
 S - print as many stars as the score
 Q - quit
-"""
-
-
-def main():
-    """Get score, print result, print stars"""
-    score = -1  # This will trigger an error message if P is selected before G
+    """
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score: float = get_valid_score()
+            score: int = get_valid_score()
         elif choice == "P":
             score_status(score)
         elif choice == "S":
@@ -36,29 +33,24 @@ def main():
 
 
 def print_stars(score):
-    """Print as many stars as the score. Round down to integer"""
     for i in range(floor(score)):
         print('*', end='')
 
 
 def score_status(score):
-    """Determine the status of the score"""
     if 90 <= score <= 100:
         print("A score of", score, "is Excellent")
     elif 50 <= score < 90:
         print("A score of", score, "is Passable")
-    elif 0 <= score < 50:
-        print("A score of", score, "is Bad")
     else:
-        print("Get score first")
+        print("A score of", score, "is Bad")
 
 
 def get_valid_score():
-    """Get score and error check the score"""
-    score = float(input("Enter score: "))
+    score = int(input("Enter score: "))
     while score == "" or score < 0 or score > 100:
         print("Invalid score")
-        score = float(input("Enter score: "))
+        score = int(input("Enter score: "))
     return score
 
 
