a57d7bdd9dea060b4f511c3051026b513a9a3bfe
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-23 15:07:13 +1000

    Calculate births, deaths, and population per year

--- prac_02/gopher_population_simulator.py
+++ prac_02/gopher_population_simulator.py
@@ -3,13 +3,27 @@ A program that simulates a population of gophers
 over a ten-year period
 and displays each year's population size
 """
+import random
 
 NUMBER_OF_YEARS = 10
 
 
 def main():
-    starting_population = 1000
-    print("Welcome to the Gopher Population Simulator! \nStarting population:", starting_population, "\nYear 1\n")
+    population = 1000
+    births = 0
+    deaths = 0
+    print("Welcome to the Gopher Population Simulator! "
+          "\nStarting population:", population,
+          "\nYear 1\n")
+    for i in range(1, NUMBER_OF_YEARS, 1):
+        proportion_of_births = random.randint(10, 20) / 100
+        births = births + (population * proportion_of_births)
+        proportion_of_deaths = random.randint(5, 25) / 100
+        deaths = deaths + (population * proportion_of_deaths)
+        population = population + births - deaths
+        print(births, "gophers were born.", deaths, "died."
+        "\nPopulation:", population,
+        "\nYear", i+1,"\n")
 
 
 main()
