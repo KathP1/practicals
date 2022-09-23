533a0ef83c1c709cffe7c770711c38844465b76a
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-23 14:34:41 +1000

    Add code to convert imported temps to fahrenheit

--- prac_02/convert_temps.py
+++ prac_02/convert_temps.py
@@ -13,12 +13,25 @@ def main():
     temps_input = open('temps_input.txt', 'r')
     temps_output = open('temps_output.txt', 'w')
     while choice != "Q":
+        if choice == "C":
+            convert_celsius_to_fahrenheit(temps_input, temps_output)
         if choice == "F":
-            for line in temps_input:
-                fahrenheit = float(line)
-                celsius = 5.0 / 9.0 * (fahrenheit - 32)
-                print(celsius, file=temps_output)
-            temps_output.close()
+            convert_fahrenheit_to_celsius(temps_input, temps_output)
 
+
+def convert_fahrenheit_to_celcius(temps_input, temps_output):
+    for line in temps_input:
+        fahrenheit = float(line)
+        celsius = 5.0 / 9.0 * (fahrenheit - 32)
+        print(celsius, file=temps_output)
+    temps_output.close()
+
+
+def convert_celsius_to_fahrenheit(temps_input, temps_output):
+    for line in temps_input:
+        celsius = float(line)
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print(fahrenheit, file=temps_output)
+    temps_output.close()
 
 main()
