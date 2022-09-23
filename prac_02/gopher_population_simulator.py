17442aa9aea6b6190ff77d715f497da12858a274
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-23 14:52:01 +1000

    Year 1 output

---
+++ prac_02/gopher_population_simulator.py
@@ -0,0 +1,15 @@
+"""
+A program that simulates a population of gophers
+over a ten-year period
+and displays each year's population size
+"""
+
+NUMBER_OF_YEARS = 10
+
+
+def main():
+    starting_population = 1000
+    print("Welcome to the Gopher Population Simulator! \nStarting population:", starting_population, "\nYear 1\n")
+
+
+main()
