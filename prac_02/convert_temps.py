801ffe8f4fb8228a660decc054fb3580ae643a5b
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-23 14:25:56 +1000

    Add code to import temps_input and convert to celsius

--- prac_02/convert_temps.py
+++ prac_02/convert_temps.py
@@ -10,30 +10,15 @@ Q - Quit"""
 def main():
     print(MENU)
     choice = input(">>> ").upper()
+    temps_input = open('temps_input.txt', 'r')
+    temps_output = open('temps_output.txt', 'w')
     while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = convert_celsius_to_fahrenheit(celsius)
-            print("Result: {:.2f} F".format(fahrenheit))
-        elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = convert_fahrenheit_to_celsius(fahrenheit)
-            print("Result:  {:.2f} C".format(celsius))
-        else:
-            print("Invalid option")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Thank you.")
-
-
-def convert_fahrenheit_to_celsius(fahrenheit):
-    celsius = 5.0 / 9.0 * (fahrenheit - 32)
-    return celsius
-
-
-def convert_celsius_to_fahrenheit(celsius):
-    fahrenheit = celsius * 9.0 / 5 + 32
-    return fahrenheit
+        if choice == "F":
+            for line in temps_input:
+                fahrenheit = float(line)
+                celsius = 5.0 / 9.0 * (fahrenheit - 32)
+                print(celsius, file=temps_output)
+            temps_output.close()
 
 
 main()
