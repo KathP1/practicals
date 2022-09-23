28d5303e0c5362e68d05f82f1d3158fc090baa50
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-23 12:02:28 +1000

    Tidy comments

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,6 +4,8 @@ G - get a valid (0 - 100) score
 P - print result
 S - print as many stars as the score
 Q - quit
+
+Help on empty user input from: https://stackoverflow.com/questions/19579997/how-to-catch-empty-user-input-using-a-try-and-except-in-python
 """
 
 from math import floor
@@ -38,19 +40,19 @@ def main():
 def print_stars(score):
     """Print as many stars as the score. Round down to integer"""
     try:
-        if score:
+        if score:  # Score entered as a float
             for i in range(floor(score)):
-               print('*', end='')
-        if not score:
+                print('*', end='')
+        if not score:  # Score not entered as float or is empty
             raise ValueError()
     except ValueError:
-        print ("Enter score first")
+        print("Enter score first")
 
 
 def score_status(score):
-    """Determine the status of the score"""
+    """Determine score status"""
     try:
-        if score:  # If score has been entered as a float
+        if score:  # Score entered as a float
             if 90 <= score <= 100:
                 print("A score of", score, "is Excellent")
             elif 50 <= score < 90:
@@ -59,7 +61,7 @@ def score_status(score):
                 print("A score of", score, "is Bad")
             else:
                 print("Get score first")
-        if not score:  # If score has not been entered as a float
+        if not score:  # Score not entered as float or is empty
             raise ValueError()
     except ValueError:
         print("Get score first")
@@ -68,13 +70,13 @@ def score_status(score):
 def get_valid_score():
     """Get score and error check the score"""
     try:
-        score = float(input("Enter score: "))  # raw_input in Python 2.x
-        if score:  # If score is entered as a float
+        score = float(input("Enter score: "))
+        if score:  # Score is entered as a float
             while score < 0 or score > 100:
                 print("Invalid score")
                 score = float(input("Enter score: "))
             return score
-        if not score:  # if score is not entered as a float or empty
+        if not score:  # Score not entered as float or is empty
             raise ValueError()
     except ValueError:
         print("Invalid value")
