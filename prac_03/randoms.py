cbfce09ad1db3f2d5284114f93891ad3e0ac7f50
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-27 12:43:44 +1000

    Answers to random.randint / .randrange / .uniform questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,30 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+
+# What did you see on line 1?
+# Integers
+# What was the smallest number you could have seen?
+# 5
+# What was the largest?
+# 20
+
+# What did you see on line 2?
+# Odd integers
+# What was the smallest number you could have seen?
+# 3
+# What was the largest?
+# 9
+# Could line 2 have produced a 4?
+# No
+
+# What did you see on line 3?
+# Decimals
+# What was the smallest number you could have seen?
+# 2.5
+# What was the largest?
+# 5.5
+
