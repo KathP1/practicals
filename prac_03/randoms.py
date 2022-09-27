8ddf3612fb40e4ad445eb0f65b11fcb94103d624
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-27 12:53:34 +1000

    Add code to produce random number between 1 and 100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -4,7 +4,6 @@ print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
-
 # What did you see on line 1?
 # Integers
 # What was the smallest number you could have seen?
@@ -28,3 +27,5 @@ print(random.uniform(2.5, 5.5))  # line 3
 # What was the largest?
 # 5.5
 
+# Write code to produce a random number between 1 and 100 inclusive
+print(random.uniform(1, 100))
