5276c3cd5eec5ca9c7286551e184a4b6ae2a0303
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-09-28 09:44:04 +1000

    Complete TODO sections

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -1,13 +1,28 @@
 """
 CP1404/CP5632 - Practical
 Fill in the TODOs to complete the task
+Write a program that gets an integer from the user
+and does not crash when a non-number is entered.
 """
 
 is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # TODO: this line
-    except:  # TODO - add the exception you want to catch after except
-        print("Please enter a valid integer.")
+        is_finished = True
+    except ValueError:
+        print("Invalid (not an integer)")
 print("Valid result is:", result)
+
+# From Programming Patterns:
+# is_valid_input = False
+# while not is_valid_input:
+#     try:
+#         age = int(input("Age: "))
+#         if age < 0:
+#             print("Age must be >= 0")
+#         else:
+#             is_valid_input = True
+#     except ValueError:
+#         print("Invalid (not an integer)")
+# print("Next year you will be", age + 1)
