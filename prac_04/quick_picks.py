a772b51f2bcfbecdacc88501ddbceae7f2ccde4b
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-09 09:15:51 +1000

    Add quick picks to a list

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,8 +8,9 @@ import random
 
 quick_pics = []
 number_of_quick_picks = int(input("How many quick picks? "))
+quick_pick = []
 for number in range(number_of_quick_picks):
-    quick_pick = []
+
     number = random.randint(1, 45)
     quick_pick.append(number)
-    print(quick_pick)
+print(quick_pick)
