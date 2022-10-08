d9c707b2ddfd8e3342afbab11552454b8204e002
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-09 09:23:09 +1000

    Adds 2 for loops - for each quick pick select 6 numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,9 +8,10 @@ import random
 
 quick_pics = []
 number_of_quick_picks = int(input("How many quick picks? "))
-quick_pick = []
-for number in range(number_of_quick_picks):
 
-    number = random.randint(1, 45)
-    quick_pick.append(number)
-print(quick_pick)
+for i in range(number_of_quick_picks):
+    quick_pick = []
+    for j in range(6):
+        number = random.randint(1, 45)
+        quick_pick.append(number)
+    print(quick_pick)
