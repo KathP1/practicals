2677a0bf980c3da2570f6cb25a2474487e096e18
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-09 09:08:42 +1000

    Add quick_picks.py with random number selection

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,13 @@
+"""
+A program that asks the user how many "quick picks they wish to generate
+The program then generates than many lines of output.
+Each line consists of 6 random numbers between 1 and 45
+"""
+
+import random
+
+quick_pics = []
+number_of_quick_picks = int(input("How many quick picks? "))
+for number in range(number_of_quick_picks):
+    quick_pic = random.randint(1, 45)
+    print(quick_pic)
