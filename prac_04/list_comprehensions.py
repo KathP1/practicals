cd94f9e9719082bb8006115f8df1365062d4a34d
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-08 22:35:35 +1000

    Creates a list of numbers > 9

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -40,6 +40,8 @@ print(numbers)
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+numbers_subset = [number for number in numbers if number > 9]
+print(numbers_subset)
 
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
