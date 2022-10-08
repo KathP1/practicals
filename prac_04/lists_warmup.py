ca1ce7618070b23bd73ef97f895b000362014794
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-08 19:12:59 +1000

    Prac 4 warm-up Q1

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+"""
+Prac 4 Warm-Up
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+"""What values do the following expressions have?"""
+# numbers[0] -> 3
+# numbers[-1] -> 2
+# numbers[3] -> 1
+# numbers[:-1] -> [3, 1, 4, 1, 5, 9]
+# numbers[3:4] -> 1 (should be [1] )
+# 5 in numbers -> True
+# 7 in numbers -> False
+# "3" in numbers -> False
+# numbers + [6, 5, 3] -> [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+"""Write statements to achieve the following:"""
+
+
