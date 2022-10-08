9646b66b2594afd8f7397aa5b9407be1b693dfad
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-08 21:11:24 +1000

    Simplify print_report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,17 +13,18 @@ def main():
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+def print_report(incomes):
     """Print report showing income per month and cumulative income"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    # We already have the list of incomes, so we can iterate through that
+    # enumerate adds a counter, in this case it's called month. We want the counter to start at 1
+    for month, income in enumerate(incomes, 1):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
