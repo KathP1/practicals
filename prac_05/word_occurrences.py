5713949840ea180fe5febb1955e77879ac236094
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-13 06:54:39 +1000

    Aligns the display

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -18,8 +18,10 @@ print(word_to_count)
 words = list(word_to_count.keys())
 words.sort()
 print(words)
+max_length = max(len(word) for word in list(word_to_count.keys()))+1
+print(max_length)
 for word in words:
-    print(f"{word}: {word_to_count[word]}")
+    print(f"{word:{max_length}}: {word_to_count[word]}")
 
 
 
