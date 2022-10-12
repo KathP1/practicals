632c5d60b551b34102b9283f39e5eaf3d51e1c36
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-13 06:24:24 +1000

    Counts occurence of words

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,3 +3,18 @@ Word Occurrences
 Estimate: 45 minutes
 Actual:
 """
+
+text = "this is a collection of words of nice words this is a fun thing it is"
+print(text)
+words = text.split()
+print(words)
+word_to_count = {}
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+print(word_to_count)
+
+
+
