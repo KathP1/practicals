b6c7670c0b7ea7239b4232b15fc454832a19d58b
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-13 17:39:08 +1000

    Initial code for emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,6 +7,15 @@ Actual:
 email_to_name = {}
 email = input("Email: ")
 while email != "":
-    suggested_name = email.split("@")[0].title()
-    print(suggested_name)
+    whole_name = email.split("@")[0]
+    name_parts = whole_name.split(".")
+    name = " ".join(name_parts).title()
+    email_to_name[email] = name
+    print(email_to_name)
+    name_is_correct = input(f"Is your name {name}? (Y/n) ").lower()
+    if name_is_correct == 'n':
+        correct_name = input("Name: ")
+        email_to_name[email] = correct_name
     email = input("Email: ")
+for email, name in email_to_name.items():
+    print(f"{name} ({email})")
