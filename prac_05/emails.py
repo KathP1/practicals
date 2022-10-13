2edc7618a13ee6f0eff3e0ffc522f11bce56fa8a
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-13 16:19:29 +1000

    Add emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,3 +4,9 @@ Estimate: 45 minutes
 Actual:
 """
 
+email_to_name = {}
+email = input("Email: ")
+while email != "":
+    suggested_name = email.split("@")[0].title()
+    print(suggested_name)
+    email = input("Email: ")
