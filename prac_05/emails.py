3703fc9c323b2b4a7a0f28e758f4c07c8b578a4b
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-13 17:51:03 +1000

    Create a function to extract presumed name from email address

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -4,18 +4,29 @@ Estimate: 45 minutes
 Actual:
 """
 
-email_to_name = {}
-email = input("Email: ")
-while email != "":
+
+def main():
+    """Create a dictionary of emails and names"""
+    email_to_name = {}
+    email = input("Email: ")
+
+    while email != "":
+        name = get_name_from_email(email)
+        name_is_correct = input(f"Is your name {name}? (Y/n) ").lower()
+        if name_is_correct == 'n':
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    """Extract the presumed name from the first part of the email address"""
     whole_name = email.split("@")[0]
     name_parts = whole_name.split(".")
     name = " ".join(name_parts).title()
-    email_to_name[email] = name
-    print(email_to_name)
-    name_is_correct = input(f"Is your name {name}? (Y/n) ").lower()
-    if name_is_correct == 'n':
-        correct_name = input("Name: ")
-        email_to_name[email] = correct_name
-    email = input("Email: ")
-for email, name in email_to_name.items():
-    print(f"{name} ({email})")
+    return name
+
+
+main()
