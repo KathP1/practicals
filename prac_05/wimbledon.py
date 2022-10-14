9eeb66577214c60538421497cb68c8ff1a57a34f
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-10-14 18:11:51 +1000

    Create a function to process the data

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,7 +11,16 @@ INDEX_COUNTRY = 1
 def main():
     records = load_data(FILENAME)
     print(records)
-    """Create a dictionary to store champion and the count of occurrences in the 'Champion' column of the data"""
+    champion_to_count, countries, number_of_countries = process_records(records)
+    print("Wimbeldon Champions:")
+    for champion in champion_to_count:
+        print(f"{champion}: {champion_to_count[champion]}")
+    print(f"These {number_of_countries} countries have won Wimbledon")
+    print(', '.join(sorted(countries)))
+
+
+def process_records(records):
+    """Create a dictionary to store champions and a set to store countries"""
     champion_to_count = {}  # empty dictionary to store champions and number of wins
     for record in records:
         try:
@@ -20,17 +29,12 @@ def main():
             champion_to_count[record[2]] = 1  # if champion isn't in the list add it and commence count at 1
     print(champion_to_count)
     print(f" This is {type(champion_to_count)}")
-    """Create a set of countries"""
     countries = set()  # empty set to store a set of unique country names
     for record in records:
         countries.add(record[1])  # Add Country (index 1) to the list
     print(sorted(countries), type(countries))
     number_of_countries = len(countries)
-    print("Wimbeldon Champions:")
-    for champion in champion_to_count:
-        print(f"{champion}: {champion_to_count[champion]}")
-    print(f"These {number_of_countries} countries have won Wimbledon")
-    print(', '.join(sorted(countries)))
+    return champion_to_count, countries, number_of_countries
 
 
 def load_data(filename):
