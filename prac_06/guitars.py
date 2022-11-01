70a1f96bb843b624b00ce78290e4f48e9d515915
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-01 18:50:56 +1000

    Create a guitars list and print it

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -9,16 +9,20 @@ from guitar import Guitar
 
 def main():
     """Get information on guitars and display their details"""
+    guitars =[]
     print("My guitars!")
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
         cost = float(input("Cost: "))
-        guitar = Guitar(name, year, cost)
-        print(guitar)
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar)
         name = input("Name: ")
 
     print("These are my guitars:")
+    for guitar in guitars:
+        print(guitar)
 
 
 main()
