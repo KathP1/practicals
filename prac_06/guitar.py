c1abfbd70fa78673a2734eb160ef7ce52ec12bfe
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-01 18:14:05 +1000

    Create Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,30 @@
+"""
+A class to store information for a guitar
+Estimated time: 90 minutes
+Time taken:
+"""
+from datetime import date
+
+
+class Guitar:
+    """Represent information about a guitar"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of a Guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        """Extract the age of the Guitar"""
+        current_year = date.today().year
+        age = current_year - self.year
+        return age
+
+    def is_vintage(self):
+        """Determines if Guitar is more than 50 years old"""
+        return self.get_age() >= 50
