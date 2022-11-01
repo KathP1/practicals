a83ba23ac126f537354c6d5efc3e61dd792cd2a0
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-01 18:52:43 +1000

    Use dummy data for testing

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -11,14 +11,17 @@ def main():
     """Get information on guitars and display their details"""
     guitars =[]
     print("My guitars!")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        new_guitar = Guitar(name, year, cost)
-        guitars.append(new_guitar)
-        print(new_guitar)
-        name = input("Name: ")
+    # name = input("Name: ")
+    # while name != "":
+    #     year = int(input("Year: "))
+    #     cost = float(input("Cost: "))
+    #     new_guitar = Guitar(name, year, cost)
+    #     guitars.append(new_guitar)
+    #     print(new_guitar)
+    #     name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     print("These are my guitars:")
     for guitar in guitars:
