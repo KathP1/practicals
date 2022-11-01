9a33097f8a10ff11d398cb90f2bee2f429d0639d
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-01 17:27:58 +1000

    Set up ProgrammingLanguage class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,3 +3,18 @@ Make a simple class for a programming language
 Estimated time: 1 hour
 Actual time:
 """
+
+
+class ProgrammingLanguage:
+    """Information about programming language"""
+
+    def __init__(self, typing="", reflection="", year=0):
+        """Initialise a ProgramingLanguage instance"""
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True or False if the programming language is dynamically typed or not"""
+        return self.typing == "Dynamic"
+
