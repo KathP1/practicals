b966ffa83377e055ef247e65d56213a4114d8d68
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-01 17:42:20 +1000

    Add _ _str_ _ function to the class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -8,13 +8,17 @@ Actual time:
 class ProgrammingLanguage:
     """Information about programming language"""
 
-    def __init__(self, typing="", reflection="", year=0):
+    def __init__(self, name="", typing="", reflection="", year=0):
         """Initialise a ProgramingLanguage instance"""
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        """Return a string representation of a ProgrammingLanguage object"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
     def is_dynamic(self):
         """Return True or False if the programming language is dynamically typed or not"""
         return self.typing == "Dynamic"
-
