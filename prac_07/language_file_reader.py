3815c7b2a0644c90a5661a2ddf11a7fda37db2e1
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-04 18:29:58 +1000

    Do Modifications 2,3: add another field to ProgrammingLanguage class and update any code that uses it

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -26,15 +26,16 @@ def main():
         # print(parts)  # debugging
         # Reflection is stored as a string (Yes/No) and we want a Boolean
         reflection = parts[2] == "Yes"
+        pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
     in_file.close()
 
-    language = ProgrammingLanguage("Eiffel", "Static", True, 1986)
+    language = ProgrammingLanguage("Eiffel", "Static", True, 1986, False)
     languages.append(language)
 
     # Loop through and display all languages (using their str method)
