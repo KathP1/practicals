307331a9cf516c7d5729e919fce209e89e99da3e
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-09 07:01:49 +1000

    Initiate main function.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,3 +4,35 @@ Time Estimate:5hrs
 Actual time:
 """
 
+MENU_STRING = """- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit"""
+
+
+def main():
+    print(MENU_STRING)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid menu choice")
+        print(MENU_STRING)
+        choice = input(">>> ").upper()
+
+
+main()
