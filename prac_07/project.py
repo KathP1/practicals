1f5363f6fb5bfa0d9a4a43c1bd70dd10d97b382b
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-09 10:42:54 +1000

    Define date format

--- prac_07/project.py
+++ prac_07/project.py
@@ -16,7 +16,7 @@ class Project:
 
     def __str__(self):
         """Return a string representation of a Project"""
-        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}%"
+        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}%"
 
 
     def __repr__(self):
