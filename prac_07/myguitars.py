12a04148e0472775da47d4375c0827a5e403d301
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-10 11:30:35 +1000

    Refactor opening file to a load function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,13 +8,7 @@ from prac_07.guitar import Guitar
 
 def main():
     """Read file of guitar details, save as a list of objects, display."""
-    guitars = []
-    in_file = open('guitars.csv', 'r')
-    for line in in_file:
-        parts = line.strip().split(',')
-        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-        guitars.append(guitar)
-    in_file.close()
+    guitars = load_guitars()
     print("My guitars!")
     name = input("Name: ")
     while name != "":
@@ -36,9 +30,21 @@ def main():
     else:
         print("No guitars")
 
-    with open('guitars.csv', 'w') as out_file:
+    with open('guitars.csv', 'w', encoding="utf-8-sig") as out_file:
         for guitar in guitars:
             print(repr(guitar), file=out_file)
+
+
+def load_guitars():
+    """Open guitars file and store as a list of lists"""
+    guitars = []
+    in_file = open('guitars.csv', 'r', encoding="utf-8-sig")
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        guitars.append(guitar)
+    in_file.close()
+    return guitars
 
 
 main()
