b078b4d7278a52c92c8a2016c55b62e7615ad5df
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-10 11:34:47 +1000

    Refactor add, display and save code to functions

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,6 +9,34 @@ from prac_07.guitar import Guitar
 def main():
     """Read file of guitar details, save as a list of objects, display."""
     guitars = load_guitars()
+    add_guitar(guitars)
+    if guitars:
+        display_guitars(guitars)
+    else:
+        print("No guitars")
+    save_guitars(guitars)
+
+
+def save_guitars(guitars):
+    """Save guitars to a file"""
+    with open('guitars.csv', 'w', encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            print(repr(guitar), file=out_file)
+
+
+def display_guitars(guitars):
+    """Display guitars sorted from oldest to youngest"""
+    guitars.sort()
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = " (vintage)"
+        print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
+
+
+def add_guitar(guitars):
+    """Get user input to add guitar details"""
     print("My guitars!")
     name = input("Name: ")
     while name != "":
@@ -18,21 +46,6 @@ def main():
         guitars.append(guitar_to_add)
         print(guitar_to_add, "added.")
         name = input("Name: ")
-
-    if guitars:
-        guitars.sort()
-        print("These are my guitars:")
-        for i, guitar in enumerate(guitars, 1):
-            vintage_string = ""
-            if guitar.is_vintage():
-                vintage_string = " (vintage)"
-            print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
-    else:
-        print("No guitars")
-
-    with open('guitars.csv', 'w', encoding="utf-8-sig") as out_file:
-        for guitar in guitars:
-            print(repr(guitar), file=out_file)
 
 
 def load_guitars():
