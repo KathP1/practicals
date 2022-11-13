d70a9f20d9cf7bf22daf9c7aadf6091734fa56dd
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-13 16:14:52 +1000

    Add method to up/down buttons

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -10,27 +10,31 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.properties import StringProperty
 
+MILES_TO_KM = 1.609344
+
 
 class ConvertMilesApp(App):
     """ConvertMilesApp is a Kivy App for converting miles to kilometers"""
-    message = StringProperty()
+
+    # message = StringProperty()
 
     def build(self):
         """build the Kivy app from the kv file"""
         Window.size = (600, 300)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
-        self.message = ""
+        # self.message = ""
         return self.root
 
-    def handle_update(self):
-        """Handle changes to the text input by updating the model from the view."""
-        self.message = self.root.ids.output_label.text
-
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
-        result = float(value) * 1.609344
+        """handle calculation, display result in label """
+        result = float(value) * MILES_TO_KM
         self.root.ids.output_label.text = str(f"{result:.3f}")
+
+    def handle_increment(self, increment):
+        """Handle up/down button press, increment the miles value +- 1"""
+        value = float(self.root.ids.input_miles.text) + increment
+        self.root.ids.input_miles.text = str(value)
 
 
 ConvertMilesApp().run()
