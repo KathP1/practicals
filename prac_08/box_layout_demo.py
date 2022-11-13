a9339409cb550a886892ed2ba023d06117a7d13f
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-13 10:13:45 +1000

    Set up clear button to reset text field and output label to blank

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,13 +4,20 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
-        self.title = "Box Layout Demo"
+        """Set up a GUI to greet a person by entered name"""
+        self.title = "Box Layout_Prac 08"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Print a statement when Greet button is pressed"""
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """Clear entered and displayed values"""
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
 
 
 BoxLayoutDemo().run()
