73eaf715c560ef31cb030291f9f7c60288b04532
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-20 21:13:36 +1000

    Amend SilverServiceTaxi get_fare function to call super().get_fare() from Taxi class

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -11,25 +11,25 @@ class SilverServiceTaxi(Taxi):
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
-        """Initialise a Taxi instance, based on parent class Car."""
+        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
         super().__init__(name, fuel)
         self.current_fare_distance = 0
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km * self.fanciness
 
     def __str__(self):
-        """Return a string like a Car but with current fare distance."""
+        """Return a string like a Taxi but with flagfall."""
         return f"{super().__str__()}, plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
-        """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance + self.flagfall
+        """Return the price for the SilverServiceTaxi trip."""
+        return super().get_fare() + self.flagfall
 
     def start_fare(self):
         """Begin a new fare."""
         self.current_fare_distance = 0
 
     def drive(self, distance):
-        """Drive like parent Car but calculate fare distance as well."""
+        """Drive like parent Taxi."""
         distance_driven = super().drive(distance)
         return distance_driven
