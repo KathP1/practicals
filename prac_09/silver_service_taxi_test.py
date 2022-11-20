cd4f0fcf55b00d235d76237470489c658ce0b3c0
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-20 21:20:15 +1000

    Initialise guitar.py, musician.py, and my_band.py

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -15,3 +15,4 @@ my_silver_service_taxi.drive(18)
 # 3. Print the SilverServiceTaxi's details and the current fare
 print(f"{my_silver_service_taxi}, current fare: ${my_silver_service_taxi.get_fare():.2f}")
 
+
