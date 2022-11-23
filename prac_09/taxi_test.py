18125cb97fae652c2056e5e1aa1e31a35bd96d9a
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-23 13:09:56 +1000

    Add doc string to taxi_test.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -5,19 +5,16 @@ Testing taxi class
 
 from prac_09.taxi import Taxi
 
-# 1. Create a new taxi object
-my_taxi = Taxi("Prius 1", 100)
 
-# 2. Drive the taxi 40km
-my_taxi.drive(40)
+def main():
+    """Test Taxi class"""
+    my_taxi = Taxi("Prius 1", 100)
+    my_taxi.drive(40)
+    print(f"{my_taxi}, current fare: ${my_taxi.get_fare():.2f}")
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    # Note: taxi couldn't drive 100km because there was only 60 units of fuel left
+    print(f"{my_taxi}, current fare: ${my_taxi.get_fare():.2f}")
 
-# 3. Print the taxi's details and the current fare
-print(f"{my_taxi}, current fare: ${my_taxi.get_fare():.2f}")
 
-# 4. Restart the meter and then drive the car 100 km
-my_taxi.start_fare()
-my_taxi.drive(100)
-
-# 5. Print the details and the current fare
-# Note: taxi couldn't drive 100km because there was only 60 units of fuel left
-print(f"{my_taxi}, current fare: ${my_taxi.get_fare():.2f}")
+main()
