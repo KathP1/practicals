f7446a9c762963b8e8c766abf23447fc9a57e86d
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-23 13:26:40 +1000

    Add taxi_simulator.py

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -2,7 +2,7 @@
 CP1404 Practical 09
 A taxi simulator program that uses Taxi and SilverServiceTaxi classes
 Estimated time: 5hrs
-Actual time:
+Actual time: 5hrs
 """
 
 from prac_09.taxi import Taxi
@@ -13,55 +13,91 @@ MENU_STRING = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     """A taxi simulator that gets a choice of taxi and distance, and calculates trip cost"""
-    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
-    print("Let's drive!")
+    prius = Taxi("prius", 100)
+    limo = SilverServiceTaxi("Limo", 100, 2)
+    hummer = SilverServiceTaxi("Hummer", 200, 4)
+    taxis = [prius, limo, hummer]
+    current_taxi = None
+    bill_to_date = 0
+    print("Let's drive")
     print(MENU_STRING)
     choice = input(">>> ").upper()
+
     while choice != "Q":
         if choice == "C":
             print("Taxis available:")
             display_taxis(taxis)
-            choose_taxi(taxis)
+            current_taxi = choose_taxi(taxis)
         elif choice == "D":
-            pass
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                bill_to_date = drive_taxi(bill_to_date, current_taxi)
         else:
-            print("Invalid menu choice")
+            print("Invalid option")
+        print(f"Bill to date: ${bill_to_date:.2f}")
         print(MENU_STRING)
         choice = input(">>> ").upper()
-    print(f"Total trip cost: $")
+    print(f"Total trip cost: ${bill_to_date:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
 
 
 def display_taxis(taxis):
-    """display_taxis"""
-    for i, taxi in enumerate(taxis):
-        print(f"{i} {taxi}")
+    """Display taxis"""
+    for index, taxi in enumerate(taxis):
+        print(f"{index} - {taxi}")
+
+
+def get_valid_number():
+    """Get valid taxi number"""
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            taxi_number = int(input("Choose taxi: "))
+            if taxi_number < 0:
+                print("Number must be >= 0")
+            else:
+                is_valid_input = True
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    return taxi_number
 
 
 def choose_taxi(taxis):
-    """Get index of chosen taxi"""
+    """Get index of chosen taxi and allocate as the current_taxi"""
+    try:
+        taxi_number = get_valid_number()
+        current_taxi = taxis[taxi_number]
+    except IndexError:
+        print("Invalid taxi choice")
+        current_taxi = None
+    return current_taxi
+
+
+def get_drive_distance():
+    """Get valid drive distance."""
     is_valid_input = False
     while not is_valid_input:
         try:
-            chosen_taxi = get_valid_number("Choose taxi: ")
-            current_taxi = taxis[chosen_taxi]
-            # print(taxis[chosen_taxi]) #Debugging
-            # print(f"current taxi: {current_taxi}") #Debugging
-            is_valid_input = True
-            return current_taxi
-        except IndexError:
-            print("Invalid taxi choice")
-
-
-def get_valid_number(user_prompt):
-    """Ask for a number and check if it is valid"""
-    is_valid_input = False
-    while not is_valid_input:
-        try:
-            number = int(input(user_prompt))
-            is_valid_input = True
+            driven_distance = float(input("Drive how far? "))
+            if driven_distance < 0:
+                print("Invalid number")
+            else:
+                is_valid_input = True
         except ValueError:
             print("Invalid input; enter a valid number")
-    return number
+    return driven_distance
+
+
+def drive_taxi(bill_to_date, current_taxi):
+    """Drive taxi."""
+    current_taxi.start_fare()
+    driven_distance = get_drive_distance()
+    current_taxi.drive(driven_distance)
+    print(f"Your {current_taxi.name} trip cost you ${current_taxi.get_fare():.2f}")
+    bill_to_date += current_taxi.get_fare()
+    return bill_to_date
 
 
 main()
