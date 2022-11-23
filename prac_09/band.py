68703d87d8be8da874bc0eddf88430f25d7caf39
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-23 12:59:07 +1000

    Amend play function to remove unnecessary use of comprehension

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,8 +3,6 @@ CP1404 Practical 09
 Band class
 """
 
-from prac_09.musician import Musician
-
 
 class Band:
     """Band class"""
@@ -28,26 +26,11 @@ class Band:
         self.musicians.append(musician)
 
     def play(self):
-        """Return a string showing the musician playing their first (or no) instrument."""
-        # # musicians_play = Musician.play(Musician.__repr__())
-        # # musicians = [str(musician) for musician in self.musicians]
-        # # musicians2 = [musician for musician in self.musicians]
-        # print(self.musicians)
-        # if not self.instruments:
-        #     return f"{self.name} needs an instrument!"
-        # return f"{self.name} is playing: {self.instruments[0]}"
-        #
-        # # print(musicians)
-        # # print(musicians2)
-        # # musicians.play()
-        # # print(musicians)
-        # # return musicians
-
+        """Return a string showing each musician playing their first (or no) instrument."""
         for musician in self.musicians:
             if not musician.instruments:
                 self.plays.append(f"{musician.name} needs an instrument")
             else:
                 self.plays.append(f"{musician.name} plays {musician.instruments[0]}")
-        musician_plays = '\n'.join([play for play in self.plays])
+        musician_plays = '\n'.join(self.plays)
         return musician_plays
-
