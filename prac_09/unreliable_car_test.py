4ae670d83c001ffa0d93f0714cb8085cb760ddf7
KathP1 <kathleen.paton@my.jcu.edu.au>
2022-11-23 13:31:06 +1000

    Remove extra # in unreliable_car_test.py

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -12,7 +12,7 @@ def main():
     my_unreliable_car = UnreliableCar("Prius 1", 100, 50)
     print(my_unreliable_car)
 
-    # # 2. Drive the unreliable_car 40km
+    # 2. Drive the unreliable_car 40km
     my_unreliable_car.drive(40)
 
     # 3. Print the unreliable car's details and the distance travelled
